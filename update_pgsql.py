cbb785fd61e3a506090e7fe72f2301f88451dac0
Thanet | Darnesh | Tiny <thanet.junlapon@gmail.com>
2023-04-23 00:31:33 +0700

    Add py

---
+++ update_pgsql.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass, replace
+from dotenv import load_dotenv, find_dotenv
+import os
+import psycopg2
+
+
+@dataclass
+class Tunnel:
+    name: str = ''
+    t: str = ''
+    lvl: str = ''
+    msg: str = ''
+    obj: str = ''
+    addr: str = ''
+    url: str = ''
+
+
+load_dotenv(find_dotenv())
+SYSLOG = '/var/log/syslog'
+# SYSLOG = 'host.syslog.log'
+
+
+def update_tunnels(tunnels:dict):
+    sql = '''INSERT INTO ngtunnel (name, url, addr, t) VALUES ('%s', '%s', '%s', '%s') 
+             ON CONFLICT (name) DO UPDATE SET (url, addr, t) = (EXCLUDED.url, EXCLUDED.addr, EXCLUDED.t);
+        '''
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            user    =os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS"),
+            host    =os.getenv("DB_HOST"),
+            port    =os.getenv("DB_PORT"),
+            database=os.getenv("DB_NAME"),
+        )
+        cur = conn.cursor()
+        # update PSQL
+        for tn_name, tn in tunnels.items():
+            cur.execute(sql, (tn_name, tn.url, tn.addr, tn.t))
+        # Commit the changes to DB
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return
+
+def main():
+    tunnels = {}
+    with open(SYSLOG) as f:
+        for line in f:
+            if all([w in line for w in ['started tunnel', 'ngrok.io']]):
+                tunnel_desc = line.strip().split(': ')[1]
+                tn = Tunnel()
+                for kv in tunnel_desc.split():
+                    if '=' in kv:
+                        k, v = kv.split('=')
+                        tn = replace(tn, **{k: v})
+
+                tunnels.update({tn.name: tn})
+
+    update_tunnels(tunnels)
+
+if __name__ == '__main__':
+    main()
